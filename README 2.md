7733b481e6eab330a87ca4c99d4fd7038512ae9a
Chen2023-UW <jchen2323@wisc.edu>
2023-05-01 23:49:29 -0500

    version 1

--- README 2.md
+++
@@ -1,3 +0,0 @@
-# 575unit2
-
-Includes programming practices from GEOG575 course unit 2(Week 4 to 6).
--- js/v1.js
+++ js/v1.js
@@ -1,25 +1,26 @@
-var map = L.map('map').setView([-3, -60], 3)
+var map = L.map('map').setView([-3, -60], 2)
 
 //create leafelt map
 //function to instantiate the Leaflet map
 function createMap(){
     //add OSM base tilelayer
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-    }).addTo(map);
+  L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 20
+}).addTo(map);
 
-    //call getData function
-    getData(map);
-};
+getData(map);
+}
 
 //apply oneachfeature to AJAX data
 function onEachFeature(feature, layer) {
     //no property named popupContent; instead, create html string with all properties
-    var popupContent = "";
+    var popupContent = "<p><b>Country Name:</b> " + feature.properties.Country + "</p>";
     if (feature.properties) {
         //loop to add feature property names and values to html string
         for (var property in feature.properties){
-            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+            popupContent += "<p>" + property + ": " + feature.properties[property] + " millon $</p>";
         }
         layer.bindPopup(popupContent);
     };
@@ -29,16 +30,16 @@ function onEachFeature(feature, layer) {
 // step 2: Import geoJSON data
 //function to retrieve the data and place it on the map
 function getData(map){
-    fetch("data/immtous.geojson")
+    fetch("data/export.geojson")
 		.then(function(response){
 			return response.json();
 		})
 		.then(function(json){
-            createPropSymbols(json);
+            //createPropSymbols(json);
 
             //Step 3. Add circle markers for point features to the map
             var geojsonMarkerOptions = {
-                radius: 6,
+                radius: 12,
                 fillColor: "#ff7800",
                 color: "#000",
                 weight: 1,
@@ -48,9 +49,10 @@ function getData(map){
 
             //create a Leaflet GeoJSON layer and add it to the map
             L.geoJson(json, {
-                pointToLayer: function (feature, latlng){
-                    return L.circleMarker(latlng, geojsonMarkerOptions);
-                }
+                onEachFeature: onEachFeature
+                //pointToLayer: function (feature, latlng){
+                    //return L.circleMarker(latlng, geojsonMarkerOptions);
+                //}
             }).addTo(map);
         })
 };
